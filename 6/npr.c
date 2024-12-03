13d87256ee4923e6ef387ab9f8548dde4f09033d
SC <swarchuri06@gmail.com>
2024-12-03 11:41:09 +0530

    Create npr.c

---
+++ 6/npr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fact(int n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    return (n * fact(n - 1));
+  }
+}
+
+int main() {
+  int n, r;
+  printf("\nenter n: ");
+  scanf("%d", & n);
+  printf("\nenter r: ");
+  scanf("%d", & r);
+  printf("npr = %d", (fact(n) / fact(n - r)));
+  return 0;
+}
